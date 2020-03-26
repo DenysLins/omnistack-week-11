04c5ddd488f8d21081cecc1843c5f636093cb8c4
Denys Lins <denyslins@gmail.com>
2020-03-26 17:13:17 -0300

    Add handleDeleteIncident function

--- aulas/frontend/src/pages/Logon/index.js
+++ aulas/frontend/src/pages/Logon/index.js
@@ -1,19 +1,39 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { FiLogIn } from 'react-icons/fi'
-import { Link } from 'react-router-dom'
+import { Link, useHistory } from 'react-router-dom'
 
+import api from '../../services/api'
 import './style.css'
 import heroesImg from '../../assets/heroes.png'
 import logoImg from '../../assets/logo.svg'
 
 function Logon () {
+  const [id, setId] = useState('')
+
+  const history = useHistory()
+
+  async function handleLogon (e) {
+    e.preventDefault()
+    try {
+      const response = await api.post('session', { id })
+      localStorage.setItem('ongId', id)
+      localStorage.setItem('ongName', response.data.name)
+      history.push('/profile')
+    } catch (error) {
+      console.log(error)
+      alert('Falha no logon. Tente novamente.')
+    }
+  }
+
   return (
     <div className="logon-container">
       <section className="form">
         <img src={logoImg} alt="Be the Hero" />
-        <form action="">
+        <form onSubmit={handleLogon}>
           <h1>Faça seu logon</h1>
-          <input placeholder="Sua ID" />
+          <input placeholder="Sua ID"
+            value={id}
+            onChange={e => setId(e.target.value)} />
           <button className="button" type="submit">Entrar</button>
           <Link className="back-link" to="/register">
             <FiLogIn size={16} color="#E02041" />
